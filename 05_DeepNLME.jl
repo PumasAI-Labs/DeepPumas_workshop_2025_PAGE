665d0238a4c0bf236ee74617d428e16af861192c
Niklas Korsbo <niklas.korsbo@gmail.com>
2023-10-27 10:39:17 +0000

    Refine the DeepNLME script.

--- 05_DeepNLME.jl
+++ 05_DeepNLME.jl
@@ -1,13 +1,3 @@
-"""
-10:15-10:45 DeepPumas IDR model – Automatic identification of dynamics
-
-## Exercise goals
-- Run through a DeepPumas workflow to get a feel for it.
-
-Note, we are not providing much context and detail here. We'll pick the individual workflow components apart after having run through it once.
-
-"""
-
 using DeepPumas
 using CairoMakie
 using StableRNGs
@@ -97,8 +87,8 @@ plotgrid(pred_datamodel)
 
 model = @model begin
     @param begin
-        # Define a multi-layer perceptron (a neural network) which maps from 6 inputs (2
-        # state variables + 4 individual parameters) to a single output. Apply L2
+        # Define a multi-layer perceptron (a neural network) which maps from 5 inputs (2
+        # state variables + 3 individual parameters) to a single output. Apply L2
         # regularization (equivalent to a Normal prior).
         NN ∈ MLPDomain(5, 6, 5, (1, identity); reg = L2(1.0))
         tvKa ∈ RealDomain(; lower = 0)
@@ -111,7 +101,7 @@ model = @model begin
     end
     @random begin
         η ~ MvNormal(Ω)
-        η_nn ~ MvNormal(1e-2 .* I(3))
+        η_nn ~ MvNormal(3, 0.1)
     end
     @pre begin
         Ka = tvKa * exp(η[1])
@@ -148,7 +138,7 @@ fpm = fit(
     init_params(model),
     MAP(FOCE());
     # Some extra options to speed up the demo at the expense of a little accuracy:
-    optim_options = (; iterations=100),
+    optim_options = (; iterations=300),
 )
 # Note that we only used 10 patients to train the model (unless you've tinkered with the code - something we encourage!).
 
@@ -158,6 +148,7 @@ plotgrid(pred_traindata)
 ins = inspect(fpm)
 goodness_of_fit(ins)
 
+
 # The model has succeeded in discovering the dynamical model if the individual predictions
 # match the observations well for test data.
 pred = predict(model, testpop, coef(fpm); obstimes = 0:0.1:24);
@@ -165,27 +156,42 @@ plotgrid(pred; ylabel="Outcome (Test data)")
 
 
 
-# If we discovered the actual relationship between drug and response then we should be able to
-# use the model on patients under a dosing regimen that the model was never fitted on.
-dr2 = DosageRegimen(0.5, ii=3, addl=2)
-testpop2 = synthetic_data(datamodel, dr2, p_data; nsubj = 12, obstimes=0:2:24)
-pred2 = predict(model, testpop2, coef(fpm); obstimes = 0:0.01:24);
+# If we discovered the actual relationship between drug and response then we should be able
+# to use the model on patients under a dosing regimen that the model was never fitted on.
+# Let's see what happends if we supply three low doses, wait a while and then supply two
+# high doses.
+dr2 = DosageRegimen(0.3, ii=3, addl=2)
+dr3 = DosageRegimen(1.5, time=25, ii=8, addl=1)
+testpop2 = synthetic_data(datamodel, DosageRegimen(dr2, dr3), p_data; nsubj = 12, obstimes=0:2:48)
+pred2 = predict(model, testpop2, coef(fpm); obstimes = 0:0.01:48);
 plotgrid(pred2)
 
 # We can overlay the data-generating model ipreds of this out-of-sample data
-pred_truth = predict(datamodel, testpop2, p_data; obstimes = 0:0.01:24);
-plotgrid(pred2)
+pred_truth = predict(datamodel, testpop2, p_data; obstimes = 0:0.01:48);
 plotgrid!(pred_truth; pred=false, ipred=(; color=Cycled(3), label="DataModel ipred"))
 
 
 #=
 Exercises:
 
-- How many subjects do you need for training? Re-train with different numbers
-  of training subjects and see how the model performs of test data.
+Explore freely, but if you want some ideas for what you can look at then here's a list
+
+- How many subjects do you need for training? Re-train with different numbers of training
+  subjects and see how the model performs on test data. Can you make the model better than 
+  it is here? Can you break it?
   
 - How noise-sensitive is this? Increase the noisiness (σ) in your training and
   test data and re-fit. Can you compensate for noise with a larger training
   population?
+
+- Is there some out-of-sample dose regimen that the model fails for? Why?
+  
+- Rewrite the model to include more knowledge. Flesh out the indirect response model and let
+  the NN capture only what's called EFF in the data generating model. You can stop using R as
+  an input but you'll need to change the MLPDomain definition for that.
+  
+- Change the number of random effects that's passed to the nerual network. What happens if
+  the DeepNLME model has fewer random effects than the data generating model? What happens if
+  it has more?
 
 =#
