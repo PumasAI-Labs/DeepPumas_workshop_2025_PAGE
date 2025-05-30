44998b0e6e838066d4cd5305cfd5af2b88c8a2bd
Niklas Korsbo <niklas@pumas.ai>
2025-05-30 13:05:37 +0000

    Add MeNet excercises.

--- 03_MeNets.jl
+++ 03_MeNets.jl
@@ -3,6 +3,7 @@ using Distributions
 using DeepPumas
 using CairoMakie
 set_mlp_backend(:staticflux)
+set_theme!(deep_light())
 
 ## Generate data
 ## The "observations", Y, are functions of time but there is within-subject correlation
@@ -11,13 +12,13 @@ datamodel_me = @model begin
   @param σ ∈ RealDomain(; lower=0., init=0.05)
   @random begin
     c1 ~ Uniform(0.5, 1.5)
-    c2 ~ Uniform(-1, 0)
+    c2 ~ LogNormal(-2, 1.0)
   end
-  @pre X = c1 * t / (t + exp10(c2))
+  @pre X = c1 * t / (t + (c2))
   @derived Y ~ @. Normal(X, σ)
 end
 
-sims = simobs(datamodel_me, [Subject(; id) for id in 1:112], (; σ=0.05); obstimes=0:0.05:1)
+sims = simobs(datamodel_me, [Subject(; id) for id in 1:200], (; σ=0.05); obstimes=0:0.05:1)
 trainpop_me = Subject.(sims[1:100])
 testpop_me = Subject.(sims[101:end])
 
@@ -47,7 +48,7 @@ fig
 ## Looks like we've successfully captured how the outcome Y depends on time, right?
 
 ## But, wait. We've ignored that the observations are correlated not only with
-## time but with the speficic patient we're sampling from.
+## time but with the specific patient we're sampling from.
 plotgrid(predict(fpm_t; obstimes=0:0.01:1)[1:12]; ylabel = "Y (Training data)")
 
 
@@ -102,8 +103,8 @@ But if the data quality is a bit off, then data quantity might compensate
 sims_new = simobs(
   datamodel_me, 
   [Subject(; id) for id in 1:100],  # Change the number of patients 
-  (; σ=0.25);                      # Tune the additive noise
-  obstimes=0:0.25:1                # Modify the observation times
+  (; σ=0.1);                      # Tune the additive noise
+  obstimes=0:0.1:0.5                # Modify the observation times
 )
 traindata_new = Subject.(sims_new)
 
@@ -123,7 +124,7 @@ plotgrid(pred_train[1:min(12,end)]; ylabel = "Y (training data)")
 
 # Plot test performance
 pred_test = predict(model_me, testpop_me, coef(fpm_me_2); obstimes=0:0.005:1)
-plotgrid(pred_test; ylabel="Y (Test data)")
+plotgrid(pred_test[1:12]; ylabel="Y (Test data)")
 
 
 #=
@@ -159,7 +160,7 @@ to be able to see in what way the fit fails
 
 model_me2 = @model begin
   @param begin
-    NN ∈ MLPDomain(2, 6, 6, (1, identity); reg=L2(1.)) # We now only have 2 inputs as opposed to 3 in model_me
+    NN ∈ MLPDomain(2, 10, 10, (1, identity); reg=L2(10)) # We now only have 2 inputs as opposed to 3 in model_me
     σ ∈ RealDomain(; lower=0.)
   end
   @random η ~ Normal(0, 1)
@@ -167,28 +168,21 @@ model_me2 = @model begin
   @derived Y ~ Normal.(X, σ)
 end
 
-sims_great = simobs(
-  datamodel_me,
-  [Subject(; id) for id in 1:100],
-  (; σ=0.01);
-  obstimes=0:0.05:1
-)
-great_data = Subject.(sims_great)
-
-plotgrid(great_data)
-plotgrid(great_data[1:6])
-
 fpm_me2 = fit(
   model_me2,
-  great_data,
+  trainpop_me,
   sample_params(model_me2),
   MAP(FOCE()); 
-  optim_options=(; f_tol = 1e-5, time_limit=3*60),
+  optim_options=(;f_tol = 1e-5)
 )
 
 pred_train = predict(fpm_me2; obstimes=0:0.01:1)[1:min(12, end)]
 plotgrid(pred_train; ylabel = "Y (training data)")
 
+pred_test = predict(fpm_me2, testpop_me; obstimes=0:0.01:1)[1:min(12, end)]
+plotgrid(pred_test; ylabel = "Y (test data)")
+
+loglikelihood(fpm_me2.model, testpop_me, coef(fpm_me2), FOCE())
 
 #=
 The model *should* not be able to make a perfect fit here - but how did it do?
@@ -216,3 +210,159 @@ begin
   fig
 end
 
+
+#=
+Let's place that in a bit of "normal" NLME modelling context.
+
+Say that you have a model structure and two parameters. You want to account for IIV, what do you do?
+=#
+
+model_a = @model begin
+  @param begin
+    tva ∈ RealDomain(; lower=0)
+    tvb ∈ RealDomain(; lower=0)
+    ω ∈ RealDomain(; lower=0)
+    σ ∈ RealDomain(; lower=0., init=0.05)
+  end
+  @random begin
+    η ~ Normal(0, ω)
+  end
+  @pre begin
+    a = tva * exp(η)
+    b = tvb
+    X = a * t / (t + b)
+  end
+  @derived Y ~ @. Normal(X, σ)
+end
+
+fpm_a = fit(model_a, trainpop_me, sample_params(model_a), MAP(FOCE()))
+
+pred_a = predict(fpm_a, testpop_me[1:12]; obstimes=0:0.01:1)
+plotgrid(pred_a; ylabel = "Y (training data)")
+
+
+model_b = @model begin
+  @param begin
+    tva ∈ RealDomain(; lower=0)
+    tvb ∈ RealDomain(; lower=0)
+    ω ∈ RealDomain(; lower=0)
+    σ ∈ RealDomain(; lower=0., init=0.05)
+  end
+  @random begin
+    η ~ Normal(0, ω)
+  end
+  @pre begin
+    a = tva
+    b = tvb * exp(η)
+    X = a * t / (t + b)
+  end
+  @derived Y ~ @. Normal(X, σ)
+end
+
+fpm_b = fit(model_b, trainpop_me, sample_params(model_b), MAP(FOCE()))
+
+pred_b = predict(fpm_b, testpop_me[1:12]; obstimes=0:0.01:1)
+plotgrid(pred_b)
+
+plotgrid(
+  pred_b; 
+  ylabel = "Y (training data)",
+  ipred = (; label = "ipred η on b"),
+  # pred = (; label = "pred η on a"),
+  pred = false
+)
+
+plotgrid!(
+  pred_a; 
+  ipred = (; label = "ipred η on a", color = Cycled(3)),
+  # pred = (; label = "pred η on a", color=Cycled(4)),
+  pred = false
+)
+
+
+
+model_c = @model begin
+  @param begin
+    tva ∈ RealDomain(; lower=0)
+    tvb ∈ RealDomain(; lower=0)
+    ω ∈ RealDomain(; lower=0)
+    c ∈ RealDomain(; lower=0, upper = 1)
+    σ ∈ RealDomain(; lower=0., init=0.05)
+  end
+  @random begin
+    η ~ Normal(0, ω)
+  end
+  @pre begin
+    a = tva * exp(c * η)
+    b = tvb * exp((1 - c) * η)
+    X = a * t / (t + b)
+  end
+  @derived Y ~ @. Normal(X, σ)
+end
+
+
+fpm_c = fit(model_c, trainpop_me, sample_params(model_c), MAP(FOCE()))
+
+pred_c = predict(fpm_c, testpop_me[1:12]; obstimes=0:0.01:1)
+plotgrid(pred_c)
+
+model_neural_η = @model begin
+  @param begin
+    tva ∈ RealDomain(; lower=0)
+    tvb ∈ RealDomain(; lower=0)
+    nn ∈ MLPDomain(1, 4, 4, (2, identity); reg=L2(1))
+    σ ∈ RealDomain(; lower=0., init=0.05)
+  end
+  @random begin
+    η ~ Normal(0, 1)
+  end
+  @pre begin
+    a = tva * exp(nn(η)[1])
+    b = tvb * exp(nn(η)[2])
+    X = a * t / (t + b)
+  end
+  @derived Y ~ @. Normal(X, σ)
+end
+
+fpm_neural_η = fit(
+  model_neural_η,
+  trainpop_me,
+  sample_params(model_neural_η),
+  MAP(FOCE()); 
+  optim_options=(; f_tol = 1e-5, time_limit=3*60),
+)
+
+pred_neural_η = predict(fpm_neural_η, testpop_me[1:12]; obstimes=0:0.01:1)
+plotgrid(pred_neural_η)
+
+nn = coef(fpm_neural_η).nn
+plt = lines(-2..2, x->nn(x)[1], label="a", axis = (; xlabel = "η"))
+lines!(-2..2, x->nn(x)[2], label="b")
+axislegend(plt.axis)
+plt
+
+
+loglikelihood(fpm_a)
+loglikelihood(fpm_b)
+loglikelihood(fpm_c)
+loglikelihood(fpm_neural_η)
+loglikelihood(fpm_me2)
+loglikelihood(fpm_me) # 2 random effects
+
+loglikelihood(fpm_a.model, testpop_me, coef(fpm_a), FOCE())
+loglikelihood(fpm_b.model, testpop_me, coef(fpm_b), FOCE())
+loglikelihood(fpm_c.model, testpop_me, coef(fpm_c), FOCE())
+loglikelihood(fpm_neural_η.model, testpop_me, coef(fpm_neural_η), FOCE())
+loglikelihood(fpm_me2.model, testpop_me, coef(fpm_me2), FOCE())
+loglikelihood(fpm_me.model, testpop_me, coef(fpm_me), FOCE()) # 2 random effects
+
+
+#=
+There we see 5 different models of the data where each has a single random effect even though the data is generated with two.
+
+Now, what is a random effect?
+
+And what's the difference between the random effects in these different cases?
+
+Which model is better?
+=#
